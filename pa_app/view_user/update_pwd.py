ecf02c6d6e775e67715b3927d8fbb64b31e6487f
Pscly <Pscly@outlook.com>
2021-08-21 18:40:29 +0800

    完善版本

--- pa_app/view_user/update_pwd.py
+++ pa_app/view_user/update_pwd.py
@@ -8,7 +8,7 @@ def update_pwd(old_pwd, new_pwd, config, xh="", cookies={}):
     修改密码
     """
 
-    # 如果没有cookies，则登录一下，获取cookies
+    # 如果没有cookies，则登录一下，获取cookies, 我预感这里以后要单独拆出来
     if not cookies:
         if not xh:
             return "没学号,也不传cookies，我怎么进?", 0 
@@ -30,7 +30,6 @@ def update_pwd(old_pwd, new_pwd, config, xh="", cookies={}):
         'password2': new_pwd,
         'submit1': '%C8%B7%C8%CF'   # 确认的url编码(gb2312)
     }
-    print(data)
-    re1 = r'<form ?name="?msn"? ?action="?javascript:window.history.go\(-1\)"? method="?post"?>(.*?)<'
+    re1 = r'<form ?name="?msn"? ?action=".*?"? method="?post"?>(.*?)<'
     x = res1.post_res(url, data, re1)
     return x
