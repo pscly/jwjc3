5764cce3c010765d9c62b642d73acf540f3a165f
Pscly <Pscly@outlook.com>
2021-08-21 16:22:56 +0800

    将get_1 改为了一个类

--- utils/core.py
+++ utils/core.py
@@ -8,24 +8,48 @@ def load_config_yaml(path='/config/config.yaml', mode='WAI'):
     return yaml.safe_load(open(path, 'r'))[mode]
     
 
-
-def get_1(url, cookies, re_text=None, headers=load_config_yaml()['HEADERS'], coding='gb2312'):
+class MyRes():
     '''
-    封装requests.get方法
-
-    args:
-        url: 访问地址
-        cookies: cookies
-        re_text: 可选，提供正则，自动匹配
-        headers: 可选，自己提供一个headers
-    return: res1, re后的东西
-    返回res1，想要什么就拿什么
+    封装一个获取结果的类
+    仔细想想， 可以把cookies保存到一个对象中，到时候获取和调用都很方便了
     '''
-    res1 = requests.get(url, headers=headers, cookies=cookies)
-    res1.encoding = coding
-    if re_text:
-        re_hou = re.findall(re_text,res1.text)
-        return res1, re_hou
-    return res1, None
+    def __init__(self, config, header={}, coding='gb2312', cookies={}) -> None:
+        self.config = config
+        self.header = header
+        self.coding = coding
+        self.cookies = cookies
+    
+    def get_res(self, url, re_text=None):
+        '''
+        封装requests.get方法
+        args:
+            url: 访问地址
+            cookies: cookies
+            re_text: 可选，提供正则，自动匹配
+            headers: 可选，自己提供一个headers
 
+        return: res1, re后的东西
+        返回res1，想要什么就拿什么
+        '''
+        res1 = requests.get(url, headers=self.headers, cookies=self.cookies)
+        res1.encoding = self.coding
+        res1.cookies = res1.cookies.get_dict()
+        res1.headers = res1.headers
+        if re_text:
+            re_hou = re.findall(re_text,res1.text)
+            return res1, re_hou
+        return res1, None
+
+    def post_res(self, url, data, re_text=None):
+        '''
+        封装requests.post方法
+        '''
+        res1 = requests.post(url, data=data, headers=self.headers, cookies=self.cookies)
+        res1.encoding = self.coding
+        res1.cookies = res1.cookies.get_dict()
+        res1.headers = res1.headers
+        if re_text:
+            re_hou = re.findall(re_text,res1.text)
+            return res1, re_hou
+        return res1, None
 
