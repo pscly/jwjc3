3b9f4b9c77b7ab5994b89547212af38dbd781d3b
Pscly <Pscly@outlook.com>
2021-09-02 14:41:05 +0800

    功能增加，现在的时间都是返回数值

--- pa_app/view_class_card/crad.py
+++ pa_app/view_class_card/crad.py
@@ -17,6 +17,35 @@ def main(s1: str):
 
     return: {userinfo}
     """
+
+    def dansuan(s1: str):
+        """
+        转化单双周的东西
+        12节双
+        return: 00000
+        00 00 0
+        01 02 0
+        第一二节， 不分单双
+        """
+        r_data = 00000
+        # 不是单双周
+        x = [x for x in s1 if x in [str(i) for i in range(10)]]
+        if len(x) == 2:
+            r_data += int(x[0]) * 1000 + int(x[1]) * 10
+        if len(x) > 2:
+            r_data += int(x[0]) * 1000 + int(x[1]) * 100 + int(x[2]) * 10 
+
+        guize = [
+            ['单', 1],
+            ['双', 2],
+        ]
+        for i in guize:
+            if i[0] in s1:
+                r_data += i[1]
+
+        return r_data
+
+
     r_data = Dict()
 
     # 人名
@@ -30,7 +59,6 @@ def main(s1: str):
     # 学年
     r_data.userinfo['stu_year'] = _[0] if (_ := re.findall(
         r'size=3>四川科技职业学院(.*?)课程表</font>', s1)) else ''
-
 
     class_data = Dict()
 
@@ -53,14 +81,18 @@ def main(s1: str):
                 for kc_data2 in kc_data1:
                     # kc_data.date = kc_data1[0][1] # 现在是直接把他当作键, 其实可以考虑121 代表12节单周
                     # 122 代表12节双周
+                    # 120 0 代表不分单双
                     kc_data[kc_data2[1]][kc_data2[4]].name = kc_data2[0]
-                    kc_data[kc_data2[1]][kc_data2[4]].date = kc_data2[1]
+                    kc_data[kc_data2[1]][kc_data2[4]].date_s = kc_data2[1]
+                    kc_data[kc_data2[1]][kc_data2[4]].date = dansuan(kc_data2[1])
                     kc_data[kc_data2[1]][kc_data2[4]].teacher = kc_data2[2]
                     kc_data[kc_data2[1]][kc_data2[4]].addr = kc_data2[3]
-                    kc_data[kc_data2[1]][kc_data2[4]].mondate = kc_data2[4]
+                    kc_data[kc_data2[1]][kc_data2[4]].mondate_s = kc_data2[4]
+                    kc_data[kc_data2[1]][kc_data2[4]].mondate = [
+                        int(i) for i in kc_data2[4].split('-')]
             class_data[zhou] |= kc_data
             zhou += 1
-    
+
     r_data.class_data = class_data
     return r_data
 
