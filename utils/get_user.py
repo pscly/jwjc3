1f197894af562be66ff5fc99f06289804e8abdb3
Pscly <Pscly@outlook.com>
2021-09-05 23:42:12 +0800

    单班级成功

--- utils/get_user.py
+++ utils/get_user.py
@@ -1,10 +1,15 @@
 # 获取一个区间内的学号,然后信息, 然后入库
-from utils import core
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from utils import logins, core
+from utils import core
+import datetime, time
+from utils import logins, core, users, kb
+from pa_app.view_user.update_pwd import update_pwd
+from pa_app.view_funcs import login_view
+from utils import view_funcs
+from pa_app.view_class_card import crad
 
 # xh = 202040030101
 
@@ -16,12 +21,15 @@ config = core.load_config_yaml(mode='WAI')
 
 def get_user(start):
     for i in range(start, start+MAX_REN):
-        x = logins.get_login_cookies(start, PWD, config)
+        x = logins.get_login_cookies(i, PWD, config)
         if not x[-1]:
             continue
-        
+        res = x[0]
+        users.get_uesr_info(res)
+        view_funcs.xueqi_xuanze(res, 1)
+        kb.get_kebiao(res)
             
 
 
 if __name__ == '__main__':
-    print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    print(get_user(202040030801))
