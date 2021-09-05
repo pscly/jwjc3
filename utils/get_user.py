30b03791751e7a7d460e5ea6a1b25c9d9960a327
Pscly <Pscly@outlook.com>
2021-09-06 00:36:13 +0800

    更新2

--- utils/get_user.py
+++ utils/get_user.py
@@ -21,15 +21,18 @@ config = core.load_config_yaml(mode='WAI')
 
 def get_user(start):
     for i in range(start, start+MAX_REN):
-        x = logins.get_login_cookies(i, PWD, config)
-        if not x[-1]:
+        try:
+            x = logins.get_login_cookies(i, PWD, config)
+            if not x[-1]:
+                continue
+            res = x[0]
+            users.get_uesr_info(res)
+            view_funcs.xueqi_xuanze(res, 1)
+            kb.get_kebiao(res)
+        except Exception as e:
+            print(e)
             continue
-        res = x[0]
-        users.get_uesr_info(res)
-        view_funcs.xueqi_xuanze(res, 1)
-        kb.get_kebiao(res)
-            
 
 
 if __name__ == '__main__':
-    print(get_user(202040030801))
+    print(get_user(202040030101))
