1ace3be8a9477405bbc4a4e1a29333c04ac04898
Pscly <Pscly@outlook.com>
2021-09-05 22:51:33 +0800

    增加了数据库模型

--- entities/models.py
+++ entities/models.py
@@ -1,18 +1,21 @@
-import datetime, sys
-sys.path('..')
+import datetime
 from sqlalchemy import create_engine    # 连接引擎
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, UniqueConstraint, Index
-from utils.core import load_config_yaml
-
 
 Base = declarative_base()   # 创建表的models， django是models.Models
+# Base = declarative_base()   # 创建表的models
 
-config = load_config_yaml()
-
-def init_db():
-    engine = create_engine(
-    config['DB'],
-    )
-
+class Users(Base):
+    __tablename__ = 'users'  # 数据库表名称
+    code = Column(String(255), primary_key=True)
+    pwd = Column(String(32))
+    name = Column(String(32))
+    gender = Column(Integer)
+    zhuanye = Column(String(32))
+    xueyuan = Column(String(32))
+    lianji = Column(Integer)
+    sfz = Column(String(32))
+    phone = Column(String(32))
+    create_time = Column(DateTime, default=datetime.datetime.now)
 
