fd2b58fa54b339821d7c9c32d4e5aa5d563c3fd4
Pscly <Pscly@outlook.com>
2021-09-08 22:33:38 +0800

    自动停止1

--- utils/get_user.py
+++ utils/get_user.py
@@ -28,7 +28,7 @@ def get_user(start):
     is_you = 0
     for i in range(1, MAX_REN+1):
         if i == 21 and is_you == 0:
-            return False    # 这个班级没人,后面就不用跑了
+            return 2    # 这个班级没人,后面就不用跑了
         try:
             x = logins.get_login_cookies(start*100+i, PWD, config)
             if not x[-1]:
@@ -36,11 +36,12 @@ def get_user(start):
             is_you = 1
             res = x[0]
             users.get_uesr_info(res)
-            view_funcs.xueqi_xuanze(res, 1)
+            view_funcs.xueqi_xuanze(res, 0)
             kb.get_kebiao(res)
         except Exception as e:
             print(e)
             continue
+    return 1
 
 def get_zhuanye(xh:int):
     """
@@ -48,12 +49,37 @@ def get_zhuanye(xh:int):
     202040030101
     xh: 20204003
     """
-    
+    x = 0
     for i in range(1, BANJI_MAX+1):
-        get_user(xh*100+i)
+        if i > 5 and x2 == 2:
+            return 2
+        x2 = get_user(xh*100+i)
+        if x2:
+            x = x2
+
+    return 1
+            
+
+def get_yuan1(xh:int):
+    """
+    给前面几位, 后面的自动补充
+    202040030101
+    xh: 202040
+    """
+    x = 0
+    for i in range(1, BANJI_MAX+1):
+        if i > 5 and x == 2:
+            return 0
+        x2 = get_zhuanye(xh*100+i)
+        if x2:
+            x = x2
+        
+    return 1
+
 
 if __name__ == '__main__':
     # print(get_user(202040090101))
-    # print(get_user(202140020201))
-    print(get_zhuanye(20204009))
+    print(get_user(2020402102))
+    # print(get_yuan1(202040))
+    # print(get_yuan1(202040030422))
     
