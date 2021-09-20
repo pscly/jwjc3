db63eaf1803b058949d89fd97d21c97e3bcf2ee8
Pscly <Pscly@outlook.com>
2021-09-21 00:22:32 +0800

    多进程搞起来了

--- utils/get_user.py
+++ utils/get_user.py
@@ -10,6 +10,7 @@ from pa_app.view_user.update_pwd import update_pwd
 from pa_app.view_funcs import login_view
 from utils import view_funcs
 from pa_app.view_class_card import crad
+from multiprocessing import Process,current_process
 
 # xh = 202040030101
 
@@ -19,6 +20,7 @@ BANJI_MAX = 10  # 最多个班级
 MAX_REN = 50    # 一个班级最大人数
 DENG_1 = 20     # 等待错误密码的
 PWD = '123456'
+P = 5   # 一次性多少个线程(一次扫描多少个学院)
 config = core.load_config_yaml(mode='WAI')
 
 def get_user(start):
@@ -51,15 +53,12 @@ def get_zhuanye(xh:int):
     xh: 20204003
     """
     x = 0
-    x2 = 0
     for i in range(1, BANJI_MAX+1):
-        if x > 3:   # 如果走了3个班都没人
-            xh += 1
-            x - 3
-            x2 += 1
-            if x2 > 3:
-                return 1
-        x += get_user(xh*100+i)
+        if x > 3:   # 如果连着走了3个班都没人
+            return 1
+        xx = get_user(xh*100+i)
+        if xx == 1:
+            x += xx
 
     return 0
             
@@ -73,21 +72,31 @@ def get_yuan1(xh:int):
     x = 0
     x2 = 0
     for i in range(1, BANJI_MAX+1):
-        if x > 3: # 如果3个专业都没人(没有这三个专业)
+        if x > 3: # 如果3个专业都没人(没有这三个专业)   # TODO 这里应该是连着的3次，而不是有三次
             xh += 1
             x - 3
             x2 += 1
             if x2 > 3:
                 # return 1
                 xh += 0
-        x += get_zhuanye(xh*100+i)
-        
+        xx = get_zhuanye(xh*100+i)
+        if xx == 1:
+            x += xx
+        else:
+            x = 0
+
     return 0
 
 
 if __name__ == '__main__':
     # print(get_user(202040090101))
-    print(get_zhuanye(20214021))
-    # print(get_yuan1(202039))
+    # pri   nt(get_zhuanye(20214021))
+    # print(get_zhuanye(20204404))
+    
+    # print(get_yuan1(202047))
     # print(get_yuan1(202040030422))
+    c = 202030
+    for i in range(c, c + P):                         # 循环10次，创建10次(个)进程
+        p = Process(target=get_yuan1, args=(i,))     # 创建1个进程，这个进程执行pin1函数
+        p.start()                                 # 启动这个进程
     
